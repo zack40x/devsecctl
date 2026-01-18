ca54ac112f796f93e5f5475fead60b55c9aa723e
zack40x <zakariaswaidan@outlook.com>
2026-01-18 19:26:01 +0200

    Homebrew: update formula to v0.1.1

--- Formula/devsecctl.rb
+++ Formula/devsecctl.rb
@@ -1,13 +1,15 @@
+
+
+
 class Devsecctl < Formula
   desc "macOS & Linux dev + security control center CLI (ports, docker helpers, snapshots, redaction)"
-  homepage "https://github.com/REPLACE_ME/devsecctl"
+  homepage "https://github.com/zack40x/devsecctl"
   license "MIT"
 
-  # After you push + create a release tarball, replace the url+sha256 below.
-  # url "https://github.com/REPLACE_ME/devsecctl/archive/refs/tags/v0.1.0.tar.gz"
-  # sha256 "REPLACE_WITH_SHA256"
+  url "https://github.com/zack40x/devsecctl/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "05a723f901da5928fa7395cf610b4e2e0196ce09013afc58f2a62e943895608d"
 
-  head "https://github.com/REPLACE_ME/devsecctl.git", branch: "main"
+  head "https://github.com/zack40x/devsecctl.git", branch: "main"
 
   def install
     libexec.install Dir["modules"]
